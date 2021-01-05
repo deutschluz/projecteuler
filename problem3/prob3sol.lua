37f70357024b51af83131d961ea71480a93b5825
deutschluz <deutschluz@gmail.com>
2021-01-04 18:56:29 -0800

    added implementation of sieve of eratosthenes

--- problem3/prob3sol.lua
+++ problem3/prob3sol.lua
@@ -8,7 +8,26 @@
 
 --Solution
 
---1. generate list of primes < sqrt(limit)
+--1. generate list of primes < limit
+function genPrime(limit)
+  --this is basically the sieve of eratosthenes
+  local l={}  --hold booleans
+  --create a list of booleans by default set to true
+  for i=1,limit do
+    l[i]=true
+  end
+  -- mark all multiples as false
+  for i=2,limit do
+    for j=i,limit do
+      l[i*j]=false
+    end
+  end
+  return l
+end
+
+result=genPrime(100)
+print("is 97 prime?",result[97])
+
 
 --2. for each p in listOfprimes
 --  if p divides limit
