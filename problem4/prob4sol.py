1898daade6afe98598d940722b772ac66d32c5ef
deutschluz <deutschluz@gmail.com>
2021-01-05 19:05:44 -0800

    adding python solution

--- problem4/prob4sol.py
+++ problem4/prob4sol.py
@@ -17,3 +17,51 @@
 #  return result
 #
 # find largest element in array 
+
+
+#function that determines whether a number is a palindrome
+def isPalindrome(num):
+    lo,hi=0,0
+    #convert num to list
+    listnum=[int(x) for x in str(num)]
+    hi=len(listnum)-1
+    while lo<hi:
+      if listnum[lo]!=listnum[hi]:
+        return False;
+      lo=lo+1
+      hi=hi-1
+#if it makes out of the while loop it is a palindrome
+    return True;
+        
+
+# function to generate products
+def genProds(lolim,uplim):
+    product=0
+    result=[] #empty list for palindromic numbers
+    for i in range(uplim,lolim,-1):
+      for j in range(uplim,lolim,-1):
+          product=i*j
+          #if product is palidrome
+          if isPalindrome(product):
+              #add them to 
+              result.append(product)
+    return result
+
+def findMax(l):
+    return max(l);
+
+def main():
+    n1=9009
+    print("9009 a palindrome",isPalindrome(n1))
+    n2=9001
+    print("9001 a palindrome",isPalindrome(n2))
+    n3=12321
+    print("12321 a palindrome",isPalindrome(n3))
+    res=genProds(100,999)
+    pali=findMax(res)
+    print("the max palindrome is",pali)
+
+    return
+
+
+main()
