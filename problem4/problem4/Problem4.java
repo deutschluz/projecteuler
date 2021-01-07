c081b4161b9c6e8b6b1aaafa66e42b2606c518b5
What should I put here? Help me out <74371276+xpqx@users.noreply.github.com>
2021-01-06 17:12:58 -0800

    problem 4 fixed

--- problem4/problem4/Problem4.java
+++ problem4/problem4/Problem4.java
@@ -1,37 +1,30 @@
 package problem4;
 
-/*
- * 
- *  A palindromic number reads the same both ways. 
-    The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-    Find the largest palindrome made from the product of two 3-digit numbers.
- * 
- * OUTPUT
- * 485584 
- * SOLVED!
- * 
- */
-
-
 public class Problem4 {
-    
-    public static void makePalindrome(int a) {
-        int resultProd = 0;
-        int i = a;
-        for(int i ; i > 0; i--) {
+
+	public static void main(String[] args){
+        int answer = createPalindrome();
+        System.out.println(answer);
+    }	
+	
+	public static int createPalindrome() {
+        int palinSol = 0;
+        //for(int i = 0; i <= 999; i++) {
+        for(int i = 999; i > 0; i--) {
+        	//for(int j = 0; j <= 999; j++) {
             for(int j = 999; j > 0; j--) {
-                int prod = ((i*j)/2);
-                if (prod <= resultProd) {
+                int result = i*j;
+                if (result <= palinSol) {
                     break;
                 }
-                String s1 = Integer.toString(prod);
+                String s1 = Integer.toString(result);
                 String s2 = new StringBuffer(s1).reverse().toString();
                 if(s1.equals(s2)) {
-                    resultProd = prod;
+                    palinSol = result;
                 }
             }       
         }
-        System.out.print(resultProd);
+        return palinSol;
     }
+    
 }
